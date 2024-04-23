aa8beea1aee03d425fdc8b806ec21a6571f35154
Samuel Yohannes <127985871+yosami14@users.noreply.github.com>
2024-04-23 10:08:15 +0300

    Minor fix on apps

--- techsync/projects/apps.py
+++ techsync/projects/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CoreConfig(AppConfig):
+class ProjectsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core'
+    name = 'projects'
