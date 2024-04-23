c0bcc7314476518966418ac2c86c40ca20c8a827
Samuel Yohannes <127985871+yosami14@users.noreply.github.com>
2024-04-23 11:35:45 +0300

    Project views and templates minor change

--- techsync/projects/urls.py
+++ techsync/projects/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('projects/', views.projects, name='projects'),
+    path('project_detail/<str:pk>/', views.project_detail, name='project_detail'),
+
 ]
--- techsync/projects/views.py
+++ techsync/projects/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-
+from .models import Project
 
 # Create your views here.
 def projects(request):
-    return render(request,'projects/projects.html')
+    projects = Project.objects.all()
+    context = {
+        'projects': projects,
+
+    }
+    return render(request,'projects/projects.html',context )
+
+def project_detail(request, pk):
+    project = Project.objects.get(id=pk)
+    tags = project.tags.all()
+    context = {
+        'project': project,
+        'tags': tags,
+    }
+    return render(request, 'projects/project_detail.html', context)
