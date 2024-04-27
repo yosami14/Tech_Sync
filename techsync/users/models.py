6bc0f3a17a400bc81607cdd8cd9272c27ca91ed8
Samuel Yohannes <127985871+yosami14@users.noreply.github.com>
2024-04-27 12:29:38 +0300

    Created profile model in user app

--- techsync/users/models.py
+++ techsync/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+import uuid
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     is_recruiter = models.BooleanField(default=False)
@@ -8,3 +8,20 @@ class User(AbstractUser):
     has_resume = models.BooleanField(default=False)
     has_company = models.BooleanField(default=False)
 
+class Profile(models.Model):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, unique=True, editable=False, )
+    user = models.OneToOneField(User, on_delete=models.CASCADE,null=True,blank=True)
+    name = models.CharField(max_length=200, blank=True,null=True)
+    username = models.CharField(max_length=200, blank=True,null=True,unique=True)
+    short_intro = models.CharField(max_length=200, blank=True,null=True)
+    bio = models.TextField(blank=True, null=True)
+    profile_image = models.ImageField(null=True, blank=True,upload_to='images/profiles/', default='images/default_profile')
+    github_link = models.CharField(max_length=200, blank=True,null=True)
+    twitter_link = models.CharField(max_length=200, blank=True,null=True)
+    linkedin_link = models.CharField(max_length=200, blank=True,null=True)
+    youtube_link = models.CharField(max_length=200, blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    # or add feature that lets user create there own link
+
+    def __str__(self):
+        return str(self.user.username)
