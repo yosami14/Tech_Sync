bb4d87e5f72cd96f36ad832f1a9b4c1dc6acef7d
Samuel Yohannes <127985871+yosami14@users.noreply.github.com>
2024-05-06 19:16:49 +0300

    Refactor CSS styles, login form, app.js, forms.py, and navbar.html templates in the techsync app

--- techsync/users/forms.py
+++ techsync/users/forms.py
@@ -8,3 +8,8 @@ class UserForm(UserCreationForm):
         labels = {
             'first_name': 'Full Name',
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+                field.widget.attrs.update({'class': 'input'})
