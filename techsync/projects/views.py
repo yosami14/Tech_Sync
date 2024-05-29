b0ba74ae005c88d5a52449a83c8cbc976af5db00
Samuel Yohannes <127985871+yosami14@users.noreply.github.com>
2024-05-29 12:44:23 +0300

    Refactor pagination functionality in projects, profiles, and users apps

--- techsync/projects/views.py
+++ techsync/projects/views.py
@@ -9,7 +9,7 @@ from .utils import get_project_search_results, paginateProjects
 def projects(request):
     search_query = request.GET.get('search_query', '')
     projects = get_project_search_results(search_query)
-    custom_range, projects = paginateProjects(request, projects, 6)
+    custom_range, projects = paginateProjects(request, projects,3 )
 
     context = {
         'projects': projects,
