ade060acbda6f24ddd0fa34898fe6be5c81c83e8
Samuel Yohannes <127985871+yosami14@users.noreply.github.com>
2024-06-04 15:37:02 +0300

    Add email configuration for SMTP server, Add Email functionality at create signal

--- techsync/techsync/settings.py
+++ techsync/techsync/settings.py
@@ -143,3 +143,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #Defined the custom user(Abstract user) at userapp -> models
 AUTH_USER_MODEL = 'users.User'
 AUTHENTICATION_BACKENDS = ['users.backends.EmailAuthBackend']
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'yosami2020@gmail.com'
+EMAIL_HOST_PASSWORD = 'rxdu uite hxsv zzdl'
--- techsync/users/signals.py
+++ techsync/users/signals.py
@@ -1,9 +1,12 @@
 def deleteUser(sender, instance, **kwargs):
     if instance.user_id is not None:
         instance.user.delete()#single signal for both User and Profile
+        
 from django.db.models.signals import post_save,post_delete
 from django.dispatch import receiver
 from .models import User, Profile
+from django.core.mail import send_mail
+from django.conf import settings
 #Single signal for both User and Profile
 def createProfile(sender, instance, created, **kwargs):
     print('Signal Triggered')
@@ -12,6 +15,27 @@ def createProfile(sender, instance, created, **kwargs):
             user=instance,
             username=instance.username,
             name=instance.first_name,
+        )
+
+        subject = f'Welcome to TechSync, {instance.first_name}!'
+        message = (
+            f'Dear {instance.first_name},\n\n'
+            'Welcome to TechSync!\n\n'
+            'We are thrilled to have you join our community. TechSync is a platform designed to help you share your experiences, '
+            'collaborate on projects, and grow your career. We believe that with your unique skills and insights, our community '
+            'will be even stronger.\n\n'
+            'If you have any questions or need assistance, please don’t hesitate to reach out to us at support@techsync.com. '
+            'We are here to help.\n\n'
+            'Best regards,\n'
+            'The TechSync Team'
+        )
+
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [instance.email],
+            fail_silently=False,
         )
 
 #update user and profile signal
