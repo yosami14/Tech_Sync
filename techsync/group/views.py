ac469de377986ff8f74175ad4a28c83878a9f4df
Samuel Yohannes <127985871+yosami14@users.noreply.github.com>
2024-06-15 11:39:58 +0300

    Refactor group forms, URLs, views, and templates in the group app

--- techsync/group/views.py
+++ techsync/group/views.py
@@ -50,7 +50,9 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
-            form.save()
+            room = form.save(commit= False)
+            room.host = request.user
+            room.save()
             return redirect('home-group')
 
     context = {
@@ -104,3 +106,13 @@ def deleteMessage(request, pk):
         return redirect('home-group')
     context = {'object': message}
     return render(request, 'main/delete_template.html', context)
+
+
+# def userProfile(request, pk):
+#     user = User.objects.get(id=pk)
+#     rooms = user.room_set.all()
+#     room_messages = user.message_set.all()
+#     topics = Topic.objects.all()
+#     context = {'user': user, 'rooms': rooms,
+#                'room_messages': room_messages, 'topics': topics}
+#     return render(request, 'base/profile.html', context)
