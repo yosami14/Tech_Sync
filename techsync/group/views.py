2dbd32e1fcb8f162f30aa1c5c964da5df7f1264f
Samuel Yohannes <127985871+yosami14@users.noreply.github.com>
2024-06-17 18:28:58 +0300

    Refactor group views and templates, and add createRoom and updateRoom functionality in the group app

--- techsync/group/views.py
+++ techsync/group/views.py
@@ -47,16 +47,22 @@ def room(request, pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit= False)
-            room.host = request.user
-            room.save()
-            return redirect('home-group')
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description')
+        )
+
+        return redirect('home-group')
 
     context = {
         'form': form,
+        'topics': topics,
         }
     return render(request, 'group/room_form.html', context)
 
@@ -65,6 +71,7 @@ def createRoom(request):
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+    topics = Topic.objects.all()
     if request.user != room.host:
         return HttpResponse('You are not allowed to view this page.')
 
@@ -78,6 +85,7 @@ def updateRoom(request, pk):
     context = {
         'room': room,
         'form': form,
+        'topics': topics,
         }
     return render(request, 'group/room_form.html', context)
 
