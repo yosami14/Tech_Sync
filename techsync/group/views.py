22881edbd9e6be22ab62a49e47375bd37e58eb92
Samuel Yohannes <127985871+yosami14@users.noreply.github.com>
2024-06-17 20:07:13 +0300

    Refactor ordering in Message model and search functionality in group views and templates

--- techsync/group/views.py
+++ techsync/group/views.py
@@ -5,12 +5,19 @@ from django.db.models import Q
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
+from django.db.models.functions import Lower
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
-    rooms = Room.objects.filter(
-        Q(topic__name__icontains=q) |
-        Q(name__icontains=q) |
-        Q(description__icontains=q)
+    q = q.lower()  # convert query to lowercase
+    rooms = Room.objects.annotate(
+        lower_topic=Lower('topic__name'),
+        lower_name=Lower('name'),
+        lower_description=Lower('description')
+    ).filter(
+        Q(lower_topic__icontains=q) |
+        Q(lower_name__icontains=q) |
+        Q(lower_description__icontains=q)
     )
     topics = Topic.objects.all()
     room_count = rooms.count()
