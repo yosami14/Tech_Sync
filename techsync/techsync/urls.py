b8264606f35eb21064835e0d2f5e06a3b16301f8
Samuel Yohannes <127985871+yosami14@users.noreply.github.com>
2024-06-18 10:59:56 +0300

    Add password reset functionality to the login page and update URLs in urls.py and event app

--- techsync/techsync/urls.py
+++ techsync/techsync/urls.py
@@ -20,11 +20,25 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/',include('projects.urls')),
     path('group/',include('group.urls')),
     path('',include('users.urls')),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="main/reset_password.html"),
+        name="reset_password"),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="main/reset_password_sent.html"),
+         name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="main/reset.html"),
+         name="password_reset_confirm"),
+
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="main/reset_password_complete.html"),
+         name="password_reset_complete"),
 ]
 
 if settings.DEBUG:
